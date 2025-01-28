69c22e3a7e264513726b24b27435672d308e1287
Jeremy Brown <chase208@4JLT.com>
2025-01-28 16:52:57 -0500

    Adding some styling

--- src/app/elements/components/periodic-table/periodic-table.component.ts
+++ src/app/elements/components/periodic-table/periodic-table.component.ts
@@ -11,8 +11,21 @@ import { element } from '../../models/element.model';
 export class PeriodicTableComponent implements OnInit {
     constructor(private elementService: ElementService) { }
     elements!: element[];
+    loadingPhrase!: string;
+
+    loadingPhrases = [
+      "Cooking up some noble gases...",
+      "I'd tell you a joke, but... all the good ones Argon.",
+      "Loading... because even Helium needs a moment to lift off.",
+      "Periodic patience... we're still in our element.",
+      "Getting our electrons together... they're being a bit negative.",
+      "Arranging the table... someone spilled the alkali metals again!",
+      "Hold on, we're splitting atoms to get this ready!",
+      "One moment please. Sodium and Chloride got a bit explosive."
+    ]
   
     ngOnInit() {
+      this.loadingPhrase = this.loadingPhrases[Math.floor(Math.random() * this.loadingPhrases.length)];
       this.elementService.getElements().subscribe(elements => {
         this.elements = elements.sort((a, b) => a.atomicNumber - b.atomicNumber);
       });
--- src/app/elements/models/element.model.ts
+++ src/app/elements/models/element.model.ts
@@ -1,5 +1,8 @@
 export interface element {
     atomicNumber: number;
+    displayColumn: number;
+    displayRow: number;
     element: string;
     symbol: string;
+    type: string;
 }
