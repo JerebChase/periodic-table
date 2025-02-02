0e6ffcd5398882571d73e784161d6cb1f3abf4f4
Jeremy Brown <chase208@4JLT.com>
2025-02-02 09:28:15 -0500

    Fixing the periodic table api for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://periodictable-api.prod.jeremychasebrown.com'
+    baseUrl: 'https://periodictable-api.jeremychasebrown.com'
   };
